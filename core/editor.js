adb2b07381aa185ecf7a52fb67933b017f72fb96
Kajetan Litwinowicz <k.litwinowicz@cksource.com>
2018-09-24 16:45:53 +0200

    Docs: added entry on focusOptions.preventScroll property.

--- core/editor.js
+++ core/editor.js
@@ -1270,7 +1270,7 @@
 		/**
 		 * Moves the selection focus to the editing area space in the editor.
 		 *
-		 * ***Since 4.11.0***
+		 * * ***Since 4.11.0***
 		 * Takes optional param `focusOptions` which holds property `preventScroll`. If set to true will prevent scrolling on focus.
 		 *
 		 * Example:
@@ -1281,6 +1281,7 @@
 		 * ```
 		 *
 		 * @param {Object} [focusOptions]
+		 * @param {Boolean} [focusOptions.preventScroll] Whenever after focus scroll should be prevented.
 		 */
 		focus: function( focusOptions ) {
 			this.fire( 'beforeFocus', { focusOptions: focusOptions } );
